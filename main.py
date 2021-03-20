60a3deb7b3c218bcd8bdcba744e2f745a7e796b7
Kiarash Vosough <vosough.k@gmail.com>
2021-03-20 20:46:38 +0330

    port scanning implemented in multi-thread. SIGINT event handler added.

--- main.py
+++ main.py
@@ -2,14 +2,19 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+from customRange import customRange
 from portSniffer import portSniffer
 import sys
 import socket
 
-
 #     # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    x = portSniffer(socket.gethostbyname('142.250.179.206'), range(1, 500), 5, 6)
-    x.start()
+    try:
+        x = portSniffer(socket.gethostbyname('142.250.179.206'), customRange(430, 450), 2, 6)
+        x.start()
+    except KeyboardInterrupt:
+        print('\tInterrupted')
+        sys.exit(0)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- portSniffer.py
+++ portSniffer.py
@@ -1,7 +1,5 @@
-import sys
 import socket
-import threading
-from datetime import datetime
+from threading import Timer, Thread
 
 
 class portSniffer:
@@ -9,28 +7,57 @@ class portSniffer:
     def __init__(self, address, portRange, waitingTime, threadNum):
         self.address = str(address)
         self.portRange = portRange
-        self.waitingTime = waitingTime
+        # self.waitingTime = waitingTime
         self.threadNum = threadNum
+        self.last_port_check_attemp = portRange.start
+        socket.setdefaulttimeout(waitingTime)
+        self.excuting_thread = []
+        self.end = False
 
     def start(self):
-        for port in self.portRange:
-            x = threading.Thread(target=self.check, args=(port,))
-            x.start()
+        while True:
+            count = 0
+            for thread in self.excuting_thread:
+                if thread.is_alive():
+                    count += 1
+            if count == 0 and self.end:
+                exit(0)
+            elif count == 0:
+                self.excuting_thread.clear()
+                self.check_and_execute()
+
+    def check_and_execute(self):
+
+        end = self.last_port_check_attemp + self.threadNum
+        start = self.last_port_check_attemp
+        if end > self.portRange.end:
+            end = self.portRange.end + 1
+            self.end = True
+
+        self.last_port_check_attemp += self.threadNum
+
+        for port in range(start, end):
+            thread = Thread(target=self.check, args=(port,))
+            thread.daemon = True
+            self.excuting_thread.append(thread)
+            thread.start()
+            thread.join(1)
 
     def check(self, port):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(1)
             flag = sock.connect_ex((self.address, port))
             if flag == 0:
                 print("Port {} is open".format(port))
-            # else:
-            #     print("Port {} is not open".format(port))
+            else:
+                print("Port {} is not open".format(port))
             sock.close()
         except socket.gaierror:
             print("\n Hostname Could Not Be Resolved !!!!")
-            # return
+            return
         except socket.error:
             print("\nServer not responding !!!!")
-            # return
-        # return
+            return
+        except KeyboardInterrupt:
+            exit(0)
+        return
