8857b78cc1802e7b3e5a8a09659b659ab2a2545b
Kiarash Vosough <vosough.k@gmail.com>
2021-03-27 21:13:30 +0430

    Port Sniifer class now use Utilities.PortManager to handle reserved ports

--- portSniffer.py
+++ portSniffer.py
@@ -1,5 +1,7 @@
 import socket
-from threading import Timer, Thread
+from threading import Thread
+
+from Utilities.portManager import reservedPortServices
 
 
 class portSniffer:
@@ -13,26 +15,6 @@ class portSniffer:
         socket.setdefaulttimeout(waitingTime)
         self.excuting_thread = []
         self.end = False
-
-    @staticmethod
-    def get_reserved_port(port):
-        dic = {'ftp': [21],
-               'telnet': [23],
-               'ssh': [22],
-               'http': [80],
-               'TLS HTTP' : [443],
-               'smtp': [25, 465, 587],
-               'SMTP & POP & IMAP With TLS': [993, 995, 465]
-               }
-
-        for key, value in dic.items():
-            if value.__contains__(port):
-                return key
-        return ''
-
-    @staticmethod
-    def get_reserved_ports():
-        return [21, 22, 23, 80, 25, 265, 287, 443, 993, 995, 465]
 
     def start(self):
         while True:
@@ -68,18 +50,16 @@ class portSniffer:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             flag = sock.connect_ex((self.address, port))
             if flag == 0:
-                if self.get_reserved_ports().__contains__(port):
-                    print("Port {} is open with {}".format(port, self.get_reserved_port(port)))
+                if reservedPortServices.contains_port(port):
+                    print("Port {} is open with {}".format(port,reservedPortServices.get_name([port])))
                 else:
                     print("Port {} is open".format(port))
-            # else:
-            #     print("Port {} is not open".format(port))
             sock.close()
         except socket.gaierror:
             print("Hostname Could Not Be Resolved !!!!")
             return
         except socket.error:
-            print("Server not responding !!!!")
+            print("Server not responding!")
             return
         except KeyboardInterrupt:
             exit(0)
