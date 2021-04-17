9150414a3032d5bc7f33c2b0b67f8f1415ff5801
Kiarash Vosough <vosough.k@gmail.com>
2021-04-17 23:31:14 +0430

    PacketModel.py is going to hold data to create packet to send.

---
+++ DataModels/PacketModel.py
@@ -0,0 +1,51 @@
+import socket
+import struct
+import threading
+from DataModels.Constant import Constant
+from Ping.CheckSumUtilities import CheckSumFactory
+from Ping.PaddingFactory import PaddingFactory
+
+
+class packetModel:
+
+    def __init__(self,
+                 destination_ip_address,
+                 packet_size,
+                 address='',
+                 ip_ver=socket.AF_INET,
+                 waiting_time=2,
+                 icmp=8,
+                 sequence_number=0,
+                 pack_format=Constant.icmp_header_format,
+                 packet_id=0):
+        self.address = address
+        self.waiting_time = waiting_time
+        self.destination_ip_address = destination_ip_address
+        self.icmp = icmp
+        self._ipVer = ip_ver
+        self.pack_format = pack_format
+        self._id = packet_id
+        self.sequence_number = sequence_number
+        self.packet_size = packet_size
+
+    def get_packet_id_based_on_thread(self):
+        if self._id == 0:
+            self._id = threading.current_thread().native_id & 0xFFFF
+        return self._id
+
+    def create_packet(self):
+
+        checksum = 0
+
+        header = struct.pack(
+            self.pack_format, self.icmp, 0, checksum, self._id, self.sequence_number
+        )
+
+        data = PaddingFactory(self.packet_size).get_padding_bytes()
+        check_sum = CheckSumFactory(header + data)
+
+        header = struct.pack(
+            self.pack_format, self.icmp, 0, check_sum.get_checksum(), self._id, self.sequence_number
+        )
+        self.sequence_number += 1
+        return header + data
