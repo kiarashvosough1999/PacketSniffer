3d6886c4a5b3470b2f7f39a765c9f5d8ddfb7f2c
Kiarash Vosough <vosough.k@gmail.com>
2021-04-21 21:33:40 +0430

    two future usable class added for better managing.

---
+++ Utilities/ExceptionManager.py
@@ -0,0 +1,17 @@
+import sys
+from Utilities.MyExceptions import MyException
+
+
+class ExceptionManager:
+
+    @staticmethod
+    def handle_exception_raw_socket(error):
+        if error == 1:
+            error_message = "Note that ICMP messages can only be send from processes running as root."
+            return MyException(error_message,
+                               error_type=MyException.socket_create_failed)
+        else:
+            type, value, traceback = sys.exc_info()
+            error_message = str(value)
+            return MyException(error_message,
+                               error_type=MyException.socket_create_failed)
---
+++ Utilities/MessageHelper.py
@@ -0,0 +1,13 @@
+class MessageHelper:
+
+    @staticmethod
+    def get_warning(message):
+        pass
+
+    @staticmethod
+    def get_success(message):
+        pass
+
+    @staticmethod
+    def get_ping_seq_result(message):
+        pass
