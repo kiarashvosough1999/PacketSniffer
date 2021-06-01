66dff3e9d08f6c0217546cbe4c3df269f8d94fb3
Kiarash Vosough <vosough.k@gmail.com>
2021-06-01 22:40:22 +0430

    implemented new method for icmpHeader to support decoding hop response.

--- DataModels/IcmpHeader.py
+++ DataModels/IcmpHeader.py
@@ -3,7 +3,7 @@ from DataModels.Constant import Constant
 from Utilities.Exception.MyExceptions import MyException
 
 
-class icmpHeader:
+class IcmpHeader:
 
     def __init__(self, code, checksum, packet_id, sequence_number, unpack_format):
         self.code = code
@@ -18,12 +18,22 @@ class icmpHeader:
     @staticmethod
     def get_icmp_header_from_packet(packet, unpack_format=Constant.icmp_header_format):
         unpacked_header = struct.unpack(unpack_format, packet[20:28])
-
         if len(unpacked_header) >= 5:
-            return icmpHeader(unpacked_header.__getitem__(1),  # code
+            return IcmpHeader(unpacked_header.__getitem__(1),  # code
                               unpacked_header.__getitem__(2),  # checksum
                               unpacked_header.__getitem__(3),  # packet_id
                               unpacked_header.__getitem__(4),  # sequence
                               unpacked_header.__getitem__(0))  # type
         raise MyException('packet header is corrupted',
                           error_type=MyException.corupted_icmp_header)
+
+    @staticmethod
+    def get_icmp_header_from_packet_for_hop(packet, unpack_format=Constant.icmp_header_format2):
+        icmp_header = packet[-8:]
+        type, code, checksum, id, sequence = struct.unpack(
+            unpack_format, icmp_header)
+        return IcmpHeader(code=code,
+                          checksum=checksum,
+                          packet_id=id,
+                          sequence_number=sequence,
+                          unpack_format=unpack_format)
