ef3b858978be6a7c3a209df6075f9d7b2e4ec13c
Kiarash Vosough <vosough.k@gmail.com>
2021-06-21 23:19:42 +0430

    added converter for int -> bytes

--- Utilities/UseFullFunction.py
+++ Utilities/UseFullFunction.py
@@ -1,3 +1,4 @@
+from DataModels.Constant import Constant
 from Utilities.Exception.MyExceptions import MyException, ExceptionAction
 
 
@@ -6,4 +7,9 @@ def safe_cast(val, to_type, error_message):
         return to_type(val)
     except (ValueError, TypeError):
         return MyException(message='{} is not {}'.format(error_message, to_type),
-                           error_type=MyException.casting_error, action=ExceptionAction.exit_0)
+                           error_type=MyException.casting_error,
+                           action=ExceptionAction.exit_0)
+
+
+def convert_int_to_bytes(integer):
+    return integer.to_bytes((integer.bit_length() + 7) // 8, Constant.big_byte_order)
